890b00a4f634a861ef0268a38bd75adf976ecf4f
Mukhammadjon0 <zokirjonovmuhammadjon0@gmail.com>
2023-06-14 20:19:20 +0500

    main-page-done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 const togglePassword = document.getElementById('togglePassword');
 const passwordInput = document.getElementById('floatingPassword');
+const loginButton = document.getElementById('loginButton');
 
 togglePassword.addEventListener('click', function () {
     const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
     passwordInput.setAttribute('type', type);
+});
+
+
+loginButton.addEventListener('click', function () {
+    window.location.href = '/Main/index.html';
 });
