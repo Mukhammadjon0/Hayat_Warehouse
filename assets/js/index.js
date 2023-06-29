9d946ff3c73be35dd1bf3bc9ef7793c6f8a6cdb3
Mukhammadjon0 <zokirjonovmuhammadjon0@gmail.com>
2023-06-29 17:56:53 +0500

    warehouse-done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,54 @@
 const officeInput = document.getElementById('office-input')
 const officeValue = document.getElementById('office-value')
-
-officeValue.onclick = () => {
+if (officeValue) {
+  officeValue.onclick = () => {
     officeInput.value = officeValue.textContent;
     officeInput.defaultValue = officeValue.textContent;
+  };
 }
 
-document.onclick = hideMenu;
-      document.oncontextmenu = rightClick;
+// document.onclick = hideMenu;
+// document.oncontextmenu = rightClick;
 
-      function hideMenu() {
-        document.getElementById("contextMenu").style.display = "none";
-      }
+// function hideMenu() {
+//   document.getElementById("contextMenu").style.display = "none";
+// }
 
-      function rightClick(e) {
-        e.preventDefault();
+// function rightClick(e) {
+//   e.preventDefault();
 
-        if (document.getElementById("contextMenu").style.display == "block")
-          hideMenu();
-        else {
-          var menu = document.getElementById("contextMenu");
+//   if (document.getElementById("contextMenu").style.display == "block")
+//     hideMenu();
+//   else {
+//     var menu = document.getElementById("contextMenu");
 
-          menu.style.display = "block";
-          menu.style.left = e.pageX + "px";
-          menu.style.top = e.pageY + "px";
-        }
-      }
+//     menu.style.display = "block";
+//     menu.style.left = e.pageX + "px";
+//     menu.style.top = e.pageY + "px";
+//   }
+// }
+
+const tableRows = document.querySelectorAll('#income-prod');
+
+// Add event listeners to table row
+tableRows.forEach(function(row) {
+  window.onclick = hideMenu; // Click event
+  row.oncontextmenu = rightClick; // Right-click event
+});
+function hideMenu() {
+  document.getElementById("contextMenu").style.display = "none";
+}
+
+function rightClick(e) {
+  e.preventDefault();
+
+  if (document.getElementById("contextMenu").style.display == "block") {
+    hideMenu();
+  } else {
+    var menu = document.getElementById("contextMenu");
+
+    menu.style.display = "block";
+    menu.style.left = e.pageX + "px";
+    menu.style.top = e.pageY + "px";
+  }
+}
